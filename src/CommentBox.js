8015942a92b67092c8d977487bb3b6d3a4c02b2a
Jakub Synowiec <jakub.synowiec@gmail.com>
2016-04-16 23:03:26 +0200

    Handle adding new comments

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -7,14 +7,36 @@ const propTypes = {
   data: PropTypes.array.isRequired,
 };
 
-function CommentBox({ data }) {
-  return (
-    <div className="commentBox">
-      <h1>Comments</h1>
-      <CommentList data={data} />
-      <CommentForm />
-    </div>
-  );
+class CommentBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: props.data,
+      lastId: props.data.slice(-1)[0].id,
+    };
+
+    this.appendComment = this.appendComment.bind(this);
+  }
+  appendComment(author, text) {
+    const lastId = this.state.lastId + 1;
+
+    this.setState({
+      data: [
+        ...this.state.data,
+        Object.assign({ id: lastId }, { author, text }),
+      ],
+      lastId,
+    });
+  }
+  render() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList data={this.state.data} />
+        <CommentForm appendComment={this.appendComment} />
+      </div>
+    );
+  }
 }
 
 CommentBox.propTypes = propTypes;
--- src/CommentForm.js
+++ src/CommentForm.js
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
-export default function CommentForm() {
-  return (
-    <div className="commentForm">
-      Hello, world! I am a CommentForm.
-    </div>
-  );
+const propTypes = {
+  appendComment: PropTypes.func.isRequired,
+};
+
+class CommentForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      author: '',
+      text: '',
+    };
+
+    this.handleAuthorChange = this.handleAuthorChange.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleAuthorChange(e) {
+    this.setState({ author: e.target.value });
+  }
+  handleTextChange(e) {
+    this.setState({ text: e.target.value });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+
+    const author = this.state.author.trim();
+    const text = this.state.text.trim();
+
+    if (!text || !author) {
+      return;
+    }
+
+    this.props.appendComment(author, text);
+    this.setState({ author: '', text: '' });
+  }
+  render() {
+    return (
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Your name"
+          value={this.state.author}
+          onChange={this.handleAuthorChange}
+        />
+        <input
+          type="text"
+          placeholder="Say something..."
+          value={this.state.text}
+          onChange={this.handleTextChange}
+        />
+        <input type="submit" value="Post" />
+      </form>
+    );
+  }
 }
+
+CommentForm.propTypes = propTypes;
+
+export default CommentForm;
